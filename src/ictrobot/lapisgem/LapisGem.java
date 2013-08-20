6470c9049b2ba7ed8158fc7b05abd4f2972cb2ab
ictrobot <ictrobot@outlook.com>
2013-08-20 17:42:32 +0100

    Test Update

--- src/ictrobot/lapisgem/LapisGem.java
+++ src/ictrobot/lapisgem/LapisGem.java
@@ -14,7 +14,7 @@ import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid = "LapisGem", name = "LapisGem", version = "1.0.0")
+@Mod(modid = "LapisGem", name = "LapisGem", version = "1.0.1")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class LapisGem {
   // The instance of your mod that Forge uses.
