f14957cc6cc11bd27eae6d8a8a6d7a933ee64188
Sciencentistguy <jamie.e.quigley@gmail.com>
2013-10-01 20:43:30 +0100

    Updated Textures

--- source/ictrobot/gems/magnetic/armor/CreativeJetpack.java
+++ source/ictrobot/gems/magnetic/armor/CreativeJetpack.java
@@ -1,18 +1,20 @@
 package ictrobot.gems.magnetic.armor;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.common.EnumHelper;
 
 public class CreativeJetpack extends ItemArmor {
 
   static EnumArmorMaterial armor = EnumHelper.addArmorMaterial("Jetpack", 0, new int[] { 0, 0, 0, 0 }, 0);
-  public static boolean upPressed = false;
   
   
   public CreativeJetpack(int par1, int par2) {
@@ -29,5 +31,14 @@ public class CreativeJetpack extends ItemArmor {
     return "Gems:textures/models/armor/Jetpack_1.png";
   }
   
-  //Flying is now done in JetpackTickHandler
-  }
+  @Override
+  public void onArmorTickUpdate (World world, EntityPlayer player, ItemStack itemStack)  {
+	player.capabilities.allowFlying = true;
+	player.sendPlayerAbilities();
+	
+	
+	
+	
+	  }
+  
+}
