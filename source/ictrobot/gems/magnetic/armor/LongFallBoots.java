394512938395348a0f2372def42c68b1ab700733
Sciencentistguy <jamie.e.quigley@gmail.com>
2013-10-03 20:45:58 +0100

    Textures

--- source/ictrobot/gems/magnetic/armor/LongFallBoots.java
+++ source/ictrobot/gems/magnetic/armor/LongFallBoots.java
@@ -13,8 +13,7 @@ import net.minecraftforge.common.EnumHelper;
 
 public class LongFallBoots extends ItemArmor {
   
-  static EnumArmorMaterial armor = EnumHelper.addArmorMaterial("Jetpack", 0, new int[] { 0, 0, 0, 0 }, 0);
-  
+  static EnumArmorMaterial armor = EnumHelper.addArmorMaterial("Jetpack", 0, new int[] { 3, 8, 6, 3 }, 10);
   public LongFallBoots(int par1, int par2) {
     super(par1, armor, par2, 3);
   }
